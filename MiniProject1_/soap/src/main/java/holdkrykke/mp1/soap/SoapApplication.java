9a78834705aff03d680f417ed9256ca9343e6efc
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-09-25 19:23:17 +0200

    Soap consume calc-service using terminal input

--- MiniProject1_/soap/src/main/java/holdkrykke/mp1/soap/SoapApplication.java
+++ MiniProject1_/soap/src/main/java/holdkrykke/mp1/soap/SoapApplication.java
@@ -1,28 +1,73 @@
 package holdkrykke.mp1.soap;
 
-import holdkrykke.mp1.client.GeoIPServiceLocator;
-import holdkrykke.mp1.client.GeoIPServiceSoap_PortType;
-import org.springframework.boot.SpringApplication;
+import holdkrykke.mp1.calc.client.CalculatorLocator;
+import holdkrykke.mp1.calc.client.CalculatorSoap_PortType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SoapApplication {
 
     public static void main(String[] args){
-        String country;
-        String ip = "2.106.16.17";
         try {
-            GeoIPServiceLocator locator	= new GeoIPServiceLocator();
-            GeoIPServiceSoap_PortType service =	locator.getGeoIPServiceSoap();
+            CalculatorLocator locator = new CalculatorLocator();
+            CalculatorSoap_PortType service = locator.getCalculatorSoap();
 
-            country	= service.getIpLocation(ip);
-            System.out.println(country);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("School SOAP free calculator");
+            System.out.println("Choose operator:\n1) Add\n2) Subtract\n3) Multiply\n4) Integer division\n5) Exit");
+
+            int operatorChoice = Integer.parseInt(scanner.nextLine());
+            int first = 0;
+            int second = 0;
+            int res = 0;
+
+            switch(operatorChoice) {
+                case 1:
+                    System.out.println("Enter first number in calculation");
+                    first = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter second number in calculation");
+                    second = Integer.parseInt(scanner.nextLine());
+                    res = service.add(first, second);
+                    System.out.println("Result is: " + res);
+                    break;
+                case 2:
+                    System.out.println("Enter first number in calculation");
+                    first = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter second number in calculation");
+                    second = Integer.parseInt(scanner.nextLine());
+                    res = service.subtract(first, second);
+                    System.out.println("Result is: " + res);
+                    break;
+                case 3:
+                    System.out.println("Enter first number in calculation");
+                    first = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter second number in calculation");
+                    second = Integer.parseInt(scanner.nextLine());
+                    res = service.multiply(first, second);
+                    System.out.println("Result is: " + res);
+                    break;
+                case 4:
+                    System.out.println("Enter first number in calculation");
+                    first = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Enter second number in calculation");
+                    second = Integer.parseInt(scanner.nextLine());
+                    res = service.divide(first, second);
+                    System.out.println("Result is: " + res);
+                    break;
+                default:
+                    System.out.println("Exiting");
+                    System.exit(0);
+            }
         }
-        catch (javax.xml.rpc.ServiceException ex) {
-            ex.printStackTrace();
+        catch (ServiceException | RemoteException ex) {
         }
-        catch (java.rmi.RemoteException	ex) {
-            ex.printStackTrace();
-        }
+    }
+
+    private void input(){
+
     }
 }
