758b94027c63ff8b51505ee66c901956ffe3c3db
Rúni Vedel Niclasen <cph-rn118@cphbusiness.dk>
2021-10-04 10:36:17 +0200

    fix student

--- MicroservicesIntroduction/students/src/main/java/com/example/demo/model/Student.java
+++ MicroservicesIntroduction/students/src/main/java/com/example/demo/model/Student.java
@@ -4,7 +4,6 @@ package com.example.demo.model;
 // add HATEOAS dependency in maven for it
 
 import javax.persistence.Entity;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
@@ -20,28 +19,34 @@ public class Student {
         super();
     }
 
-    public Student(Long id, String name, String passportNumber) {
+    public Student(Long id, String name, String mail) {
         super();
         this.id = id;
         this.name = name;
         this.mail = mail;
     }
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getMail() {
         return mail;
     }
-    public void setMail(String passportNumber) {
+
+    public void setMail(String mail) {
         this.mail = mail;
     }
 }
