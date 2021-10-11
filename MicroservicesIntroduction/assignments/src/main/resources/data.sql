ac2d532f9f7349ec0f4e6e0e999edb637b5a33b4
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-10-11 17:54:30 +0200

    assignment port change and inserts

--- MicroservicesIntroduction/assignments/src/main/resources/data.sql
+++ MicroservicesIntroduction/assignments/src/main/resources/data.sql
@@ -1,8 +1,8 @@
--- insert into student values(101, 'Ronja', 'ro@mail.dk');
--- insert into student values(102, 'Bastian', 'ba@mail.dk');
--- insert into student values(103, 'Alice', 'al@mail.wld');
--- insert into student values(104, 'Bob', 'bo@bobov.dol');
--- insert into student values(105, 'Jonathan', 'jo@mail.us');
--- insert into student values(106, 'Frida', 'fri@mail.us');
--- insert into student values(107, 'Freya', 'fre@mail.us');
+insert into assignment values(101, 'Mini Project 1', 1, 'Description 1');
+insert into assignment values(102, 'Reading Assignment 1', 2, 'Description 1');
+insert into assignment values(103, 'Mini Project 2', 3, 'Description 1');
+insert into assignment values(104, 'Reading Assignment 2', 4, 'Description 1');
+insert into assignment values(105, 'Mini Project 3', 5, 'Description 1');
+insert into assignment values(106, 'Reading Assignment 2', 6, 'Description 1');
+insert into assignment values(107, 'Exam Project', 7, 'Description 1');
 
