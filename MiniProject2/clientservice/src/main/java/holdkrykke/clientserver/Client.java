433314f8a213bfec268b0af47766d49ea3924ae0
Runi <Runiniclasen@hotmail.com>
2021-10-21 12:53:15 +0200

    update hateoas definition

--- MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/Client.java
+++ MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/Client.java
@@ -2,14 +2,14 @@ package holdkrykke.clientserver;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.hateoas.Resources;
 
 @CrossOrigin
 @FeignClient("students")
 public interface Client
 {
     @GetMapping("/students")
-    Resources<Student> readStudents();
+    CollectionModel<Student> readStudents();
 }
