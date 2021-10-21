8fb4cae1f38d5fd02a9ebb4bd1733301ed87f85f
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-10-21 16:08:19 +0200

    fix dependency hell

--- MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/Client.java
+++ MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/Client.java
@@ -1,7 +1,9 @@
 package holdkrykke.clientserver;
 
+
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
+
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
--- MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/ClientServiceApplication.java
+++ MiniProject2/clientservice/src/main/java/holdkrykke/clientserver/ClientServiceApplication.java
@@ -1,7 +1,14 @@
 package holdkrykke.clientserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEnabled;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
