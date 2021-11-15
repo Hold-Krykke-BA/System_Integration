617805be98477eb3d347d4316eefe5a0f70b4384
Runi <Runiniclasen@hotmail.com>
2021-11-15 10:24:05 +0100

    push idea for new LoanApplicant
    need name for message

--- MiniProject3/KafkaConsumer/src/main/java/holdkrykke/consumer/consumer/LoanApplicant.java
+++ MiniProject3/KafkaConsumer/src/main/java/holdkrykke/consumer/consumer/LoanApplicant.java
@@ -6,11 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
 public class LoanApplicant {
     private long ssn, creditScore, loanAmount, currentDebt, yearlySalary;
-    private String loanType, applicationID;
+    private String loanType, applicationID, firstName, lastName;
 }
