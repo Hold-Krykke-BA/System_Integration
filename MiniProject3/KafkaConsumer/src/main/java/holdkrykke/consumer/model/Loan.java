85e296b0ccf3277a854d6b42e4ea372372a11072
Runi <Runiniclasen@hotmail.com>
2021-11-15 10:25:19 +0100

    push new Loan Object for response

---
+++ MiniProject3/KafkaConsumer/src/main/java/holdkrykke/consumer/model/Loan.java
@@ -0,0 +1,38 @@
+package holdkrykke.consumer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import holdkrykke.consumer.consumer.LoanApplicant;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Loan {
+
+    private static LoanApplicant applicant;
+    /**
+     * loanMessage should be seen as a JSON field.
+     * It can be extended with more depending on field loanStatus
+     */
+    private static String loanMessage;
+    private boolean loanStatus;
+    /**
+     * ÅOP
+     */
+    private double APR;
+    /**
+     * Monthly Payment
+     */
+    private double payment;
+    private int durationInMonths;
+
+    static {
+        loanMessage = String.format("Dear Mx %s\n " +
+                "Thank you for your application to our bank. Below you will find the details of the loan.", applicant.getLastName());
+
+    }
+
+}
