21a483293f871329947c075c05dbd9c2f19df481
Runi <Runiniclasen@hotmail.com>
2021-11-30 16:22:27 +0100

    update worker

--- MiniProject4/external_worker.js
+++ MiniProject4/external_worker.js
@@ -8,9 +8,43 @@ const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 // create a Client instance with custom configuration
 const client = new Client(config);
 
-// susbscribe to the topic: 'creditScoreChecker'
-client.subscribe("topicName", async function ({ task, taskService }) {
+// susbscribe to the topic: 'topicValidation'
+client.subscribe("topicValidation", async function ({ task, taskService }) {
   // Put your business logic
   // complete the task
+
+  const amount = task.variables.get("Amount");
+  const debt = task.variables.get("Debt");
+  const name = task.variables.get("Name");
+  const age = task.variables.get("Age"); //number
+  const salary = task.variables.get("Salary"); //long
+  const status = task.variables.get("Status"); //
+
+  console.log('amount', amount, typeof(amount))
+  console.log('debt', debt, typeof(debt))
+  console.log('age', age, typeof(age))
+  console.log('salary', salary, typeof(salary))
+  console.log('status', status, typeof(status))
+
+
+  if (name) {
+    console.log('length accepted')
+    await taskService.complete(task);
+  } else {
+    console.log('denied')
+    // errorMessage?: string | undefined;
+    // errorDetails?: string | undefined;
+    // retries?: number | undefined;
+    // retryTimeout?: number | undefined;
+    // await taskService.handleFailure(task, { errorMessage: "Name too short" });
+    await taskService.handleBpmnError(task, "Error_0b790gi", "Name too short");
+  }
+});
+
+
+client.subscribe("topicCustomer", async function ({ task, taskService }) {
+  // Put your business logic
+  // complete the task
+  //todo
   await taskService.complete(task);
 });
