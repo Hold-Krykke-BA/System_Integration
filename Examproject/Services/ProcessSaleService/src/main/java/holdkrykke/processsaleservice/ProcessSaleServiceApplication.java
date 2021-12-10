ef537954a519799e648fdfa79db56558779db2b3
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-12-10 23:54:15 +0100

    commit before change of pom

--- Examproject/Services/ProcessSaleService/src/main/java/holdkrykke/processsaleservice/ProcessSaleServiceApplication.java
+++ Examproject/Services/ProcessSaleService/src/main/java/holdkrykke/processsaleservice/ProcessSaleServiceApplication.java
@@ -1,13 +1,30 @@
 package holdkrykke.processsaleservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableProcessApplication
+//@EnableKafka
+//@EnableMongoRepositories
 public class ProcessSaleServiceApplication {
-
+    @Autowired
+    private RuntimeService runtimeService;
     public static void main(String[] args) {
         SpringApplication.run(ProcessSaleServiceApplication.class, args);
     }
 
+    @EventListener
+    private void processPostDeploy(PostDeployEvent event) {
+        runtimeService.startProcessInstanceByKey("loanApproval");
+    }
 }
