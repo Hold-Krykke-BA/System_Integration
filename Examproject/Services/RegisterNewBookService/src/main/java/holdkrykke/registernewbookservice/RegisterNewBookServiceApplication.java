d895d4eafedbd9a70ee3dbaaab8e84dd429ca9c7
Runi <Runiniclasen@hotmail.com>
2021-12-10 17:21:34 +0100

    add client and test in main

--- Examproject/Services/RegisterNewBookService/src/main/java/holdkrykke/registernewbookservice/RegisterNewBookServiceApplication.java
+++ Examproject/Services/RegisterNewBookService/src/main/java/holdkrykke/registernewbookservice/RegisterNewBookServiceApplication.java
@@ -1,13 +1,25 @@
 package holdkrykke.registernewbookservice;
 
+import holdkrykke.Exceptions.ISBNValidationException;
+import holdkrykke.registernewbookservice.services.soap.ISBNValidationClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RegisterNewBookServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ISBNValidationException {
+
         SpringApplication.run(RegisterNewBookServiceApplication.class, args);
+
+        var uno = ISBNValidationClient.getInstance().validateISBN("87-01-32671-6");
+        var dos = ISBNValidationClient.getInstance().validateISBN("87-01-32671-6");
+        var tres = ISBNValidationClient.getInstance().validateISBN("NotAnISBN");
+
+        System.out.println("should be true: " + uno);
+        System.out.println("should be true: " + dos);
+        System.out.println("should be false: " + tres);
+
     }
 
 }
--- Examproject/Services/RegisterNewBookService/src/main/java/holdkrykke/registernewbookservice/services/soap/ISBNValidationClient.java
+++ Examproject/Services/RegisterNewBookService/src/main/java/holdkrykke/registernewbookservice/services/soap/ISBNValidationClient.java
@@ -1,9 +1,69 @@
-package holdkrykke.registernewbookservice.services.soap.consumingwebservice;
+package holdkrykke.registernewbookservice.services.soap;
 
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import holdkrykke.Exceptions.ISBNValidationException;
+import holdkrykke.registernewbookservice.services.soap.consumingwebservice.ISBNServiceLocator;
+import holdkrykke.registernewbookservice.services.soap.consumingwebservice.ISBNServiceSoapType;
 
-public class ISBNValidationClient extends WebServiceGatewaySupport {
-    //V1.0
-    //Continue from here if better:
-    //https://spring.io/guides/gs/consuming-web-service/#_create_a_country_service_client
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+
+/**
+ * Singleton implementation of the ISBN Validation Client of the ISBN Validation SOAP service.
+ */
+public class ISBNValidationClient {
+
+    private static ISBNValidationClient instance = new ISBNValidationClient();
+    private static ISBNServiceLocator locator;
+    private static ISBNServiceSoapType service;
+
+    private ISBNValidationClient() {
+        System.out.println("Attempting to instantiate the ISBN Validation Client.");
+        locator = new ISBNServiceLocator();
+        try {
+            service = locator.getISBNServiceSoap();
+        } catch (ServiceException e) {
+            //throw new ISBNValidationException("Could not instantiate ISBN SOAP service", e);
+            System.out.println("Unable to instantiate ISBN SOAP service:\n");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Succesfully instantiated the ISBN Validation Client!");
+    }
+
+    public static ISBNValidationClient getInstance() {
+        return instance;
+    }
+
+    /**
+     * Determines the ISBN standard by length and validates it by a remote SOAP service.
+     * <p>
+     * https://webservices.daehosting.com/services/ISBNService.wso
+     * <p>
+     * Dashes are accepted by the service.
+     * <p>
+     * Example ISBN10: 87-01-32671-6
+     * <p>
+     * Example ISBN13: 9788701326513
+     *
+     * @param ISBN The ISBN to validate
+     * @return true if valid, false if not
+     * @throws ISBNValidationException If string does not live up to ISBN rules
+     */
+    public boolean validateISBN(String ISBN) throws ISBNValidationException {
+        String ISBNNoDash = ISBN.replace("-", "");
+        try {
+            if (ISBNNoDash.length() == 13) {
+                //Rule: 13 digits number
+                return service.isValidISBN13(ISBN);
+
+            } else if (ISBNNoDash.length() == 10 || ISBNNoDash.length() == 9) {
+                //Rule: 10 digits number or 8 digits and an X.
+                return service.isValidISBN10(ISBN);
+            }
+        } catch (RemoteException e) {
+            throw new ISBNValidationException("Failed to establish remote connection.", e);
+        }
+        throw new ISBNValidationException(String.format("ISBN input %s is not a valid ISBN format", ISBN));
+    }
+
 }
