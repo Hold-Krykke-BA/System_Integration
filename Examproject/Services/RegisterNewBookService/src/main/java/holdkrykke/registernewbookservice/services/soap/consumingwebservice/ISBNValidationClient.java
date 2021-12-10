f35a072b136405f2b99e5724848fa4d5653c64de
Runi <Runiniclasen@hotmail.com>
2021-12-10 16:01:50 +0100

    Soap V1.0

---
+++ Examproject/Services/RegisterNewBookService/src/main/java/holdkrykke/registernewbookservice/services/soap/consumingwebservice/ISBNValidationClient.java
@@ -0,0 +1,9 @@
+package holdkrykke.registernewbookservice.services.soap.consumingwebservice;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+public class ISBNValidationClient extends WebServiceGatewaySupport {
+    //V1.0
+    //Continue from here if better:
+    //https://spring.io/guides/gs/consuming-web-service/#_create_a_country_service_client
+}
