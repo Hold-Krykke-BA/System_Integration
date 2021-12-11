f38ef0830d58fcca564b2989ee9c48157d12a8e6
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-12-11 21:18:15 +0100

    camunda working, needs model change and service task

--- Examproject/Services/ProcessSaleService/src/main/java/holdkrykke/processsaleservice/services/kafka/ConsumerService.java
+++ Examproject/Services/ProcessSaleService/src/main/java/holdkrykke/processsaleservice/services/kafka/ConsumerService.java
@@ -2,6 +2,8 @@ package holdkrykke.processsaleservice.services.kafka;
 
 import holdkrykke.processsaleservice.models.Order;
 import holdkrykke.processsaleservice.repositories.OrderRepository;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.variable.Variables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ConsumerService {
@@ -18,15 +22,27 @@ public class ConsumerService {
 
     private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
 
+    @Autowired
+    private RuntimeService runtimeService;
+
     @KafkaListener(topics = "saleregistered", groupId = "salegroup")
     public void consume(String message) throws IOException
     {
         System.out.println("Consumed message:" + message);
         List<Order> retrieved = orderRepository.findAll();
         for(Order order: retrieved){
-            System.out.println(order.toString());
+            System.out.println(order.getId());
+            Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("order", order);
+            runtimeService.startProcessInstanceByKey("loanApproval",Variables.createVariables() //
+                    .putValueTyped("order", Variables.objectValue(order).serializationDataFormat(Variables.SerializationDataFormats.JSON).create())
+                    .putValue("isDigital", order.getIsDigital())
+//                    .putValue(ProcessConstants.VAR_NAME_uiBaseUrl, uiBaseUrl)//
+//                    .putValue(ProcessConstants.VAR_NAME_applicationNumber, application.getApplicationNumber())//
+//                    .putValue(ProcessConstants.VAR_NAME_applicantName, application.getApplicant().getName())//
+            );
         }
 
-        //logger.info("&&& Message [{}] consumed", message);
+
     }
 }
