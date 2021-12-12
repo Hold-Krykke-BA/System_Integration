1084fb6753ee0dfa5bc8e1ba1a3424e03cf20f53
Runi <Runiniclasen@hotmail.com>
2021-12-12 17:22:12 +0100

    EnableRedisRepositories

--- Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/CacheServiceApplication.java
+++ Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/CacheServiceApplication.java
@@ -2,12 +2,22 @@ package holdkrykke.cacheservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
+//@EnableMongoRepositories
 public class CacheServiceApplication {
 
     public static void main(String[] args) {
+
+        //For this application as a whole:
+        //Request for book
+        //Check cache
+        //If in cache, refresh the TTL and return the data (done)
+        //if NOT in cache, get from external sources, put in cache, return data
+
         SpringApplication.run(CacheServiceApplication.class, args);
     }
-
 }
