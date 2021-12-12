1ffef6f04d146b98ad269012abe551772c7e4d43
Runi <Runiniclasen@hotmail.com>
2021-12-12 23:00:40 +0100

    add EnableKafka annotation

--- Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/BookServiceApplication.java
+++ Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/BookServiceApplication.java
@@ -4,9 +4,11 @@ import holdkrykke.Exceptions.ISBNValidationException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableKafka
 public class BookServiceApplication {
 
     public static void main(String[] args) throws ISBNValidationException {
