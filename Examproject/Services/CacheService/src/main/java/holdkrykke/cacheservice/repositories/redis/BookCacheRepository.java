ded78673d0236cb616b6e003b4082d886e3ba65f
Runi <Runiniclasen@hotmail.com>
2021-12-12 17:23:47 +0100

    add custom redis repository
    saveBook
    findBookByID
    findBookByIdAndRefreshExpiration

---
+++ Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/repositories/redis/BookCacheRepository.java
@@ -0,0 +1,78 @@
+package holdkrykke.cacheservice.repositories.redis;
+
+import holdkrykke.cacheservice.exceptions.NotFoundException;
+import holdkrykke.cacheservice.models.redis.BookCacheDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * This repository the default repository for Redis
+ */
+@Repository
+public class BookCacheRepository {
+    private static int _CACHE_DURATION = 300;
+    private static TimeUnit _UNIT = TimeUnit.SECONDS;
+
+    @Autowired
+    RedisTemplate<String, BookCacheDTO> redisTemplate;
+
+
+    /**
+     * Save a book to the redis cache and apply default time to live of 5 minutes
+     *
+     * @param book
+     */
+    public void saveBook(BookCacheDTO book) {
+        redisTemplate.opsForValue().set(
+                book.getId(), book, _CACHE_DURATION, _UNIT
+        );
+    }
+
+    /**
+     * Are you sure you want this and not findByIdAndRefreshExpiration?
+     *
+     * @param id
+     * @return
+     * @throws NotFoundException
+     */
+    public BookCacheDTO findBookById(String id) throws NotFoundException {
+        var keyBoundOps = redisTemplate.boundValueOps(id);
+        var result = keyBoundOps.get(); //get book, may be null
+        result.set_TimeToLive(keyBoundOps.getExpire()); //get expiry time, and attach to obj
+        return result;
+    }
+
+
+    /**
+     * Finds a book by ID and refreshes its token to the default.
+     *
+     * @param id id of the book
+     * @return Book if found, otherwise null
+     * @throws NotFoundException if unable to set expiration.
+     */
+    public BookCacheDTO findBookByIdAndRefreshExpiration(String id) throws NotFoundException {
+        var keyBoundOps = redisTemplate.boundValueOps(id);
+        BookCacheDTO book = keyBoundOps.get(); //get book, may be null
+
+        if (book == null) {
+            //Break, do not set new expiration
+            return book;
+        }
+
+        //refresh expiry timer
+        boolean success = keyBoundOps.expire(_CACHE_DURATION, _UNIT);
+        if (!success) {
+            throw new NotFoundException("Could not set new expiration for item");
+        }
+        book.set_TimeToLive(keyBoundOps.getExpire());
+        return book;
+    }
+}
+//todo
+// Expire key when changed in other part of system, i.e. when a sale has been made
+//  ^ would require a connection between Book Service & Caching service + rest endpoints. Probably not gonna happen.
+// Get many (if necessary)
