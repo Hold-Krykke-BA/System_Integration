0f54b0da4eea2b6e9515a07ad356153a1c3af71b
Runi <Runiniclasen@hotmail.com>
2021-12-12 23:17:14 +0100

    further prepare kafka

--- Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/repositories/BookStoreRepository.java
+++ Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/repositories/BookStoreRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-//todo https://stackoverflow.com/questions/22824840/when-to-use-restcontroller-vs-repositoryrestresource
 public interface BookStoreRepository extends MongoRepository<Book, String> {
+
+
 }
--- Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/services/kafka/KafkaConsumer.java
+++ Examproject/Services/BookService/src/main/java/holdkrykke/bookservice/services/kafka/KafkaConsumer.java
@@ -1,17 +1,26 @@
 package holdkrykke.bookservice.services.kafka;
 
-import org.springframework.beans.factory.annotation.Value;
+
+import holdkrykke.bookservice.repositories.BookStoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class KafkaConsumer {
+
+    @Autowired
+    private BookStoreRepository repo;
 
     @KafkaListener(topics = "saleregisteredcaching", groupId = "salegroup")
     public void consume(String message) {
         System.out.println("Consumed message: " + message);
         //Parse message to Order
         //Update DB on ID and decrement quantity by 1
+//        Book book = repo.findById("").get();
+//        book.setQuantity(book.getQuantity()-1);
+//        repo.save(book);
     }
 
 }
