a4172a9198855f5301d2a2c9feec1472916ad3ea
Camilla Staunstrup <camilla.valerius@gmail.com>
2021-12-13 10:38:49 +0100

    simple proto working

--- Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/services/grpc/ProtoService.java
+++ Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/services/grpc/ProtoService.java
@@ -5,23 +5,25 @@ import holdkrykke.cacheservice.services.RegisterRequest;
 import holdkrykke.cacheservice.services.ProtoResponse;
 import io.grpc.stub.StreamObserver;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
-
 public class ProtoService extends RegisterServiceGrpc.RegisterServiceImplBase{
 
     @Override
     public void register(RegisterRequest request, StreamObserver<ProtoResponse> responseObserver) {
         System.out.println("Request received from client:\n" + request);
 
-        String retrieved = "am a hat"; // set response to bloom
+        String retrieved = handleRequest(request); // set response to bloom
         ProtoResponse response = ProtoResponse.newBuilder()
                 .setRetrieved(retrieved)
                 .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    private String handleRequest(RegisterRequest request){
+        String parameterISBN = request.getIsbn();
+        String parameterAUTHORS = request.getAuthors();
+        String parameterTITLE = request.getTitle();
+        return "am a hat"; // response object/list as stringified
     }
 }
