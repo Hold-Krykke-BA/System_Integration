4189b41f6d15b9ea03f7967355d52ab735d89694
Runi <Runiniclasen@hotmail.com>
2021-12-15 11:38:41 +0100

    add ActiveMQConfig

---
+++ Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/services/activemq/ActiveMQConfig.java
@@ -0,0 +1,23 @@
+package holdkrykke.cacheservice.services.activemq;
+
+import org.apache.camel.component.activemq.ActiveMQComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.ConnectionFactory;
+
+@Configuration
+/**
+ * ActiveMQ config for use with Camel
+ */
+public class ActiveMQConfig {
+
+    @Bean(name = "activemq")
+    public ActiveMQComponent createComponent(ConnectionFactory factory) {
+        ActiveMQComponent activeMQComponent = new ActiveMQComponent();
+        activeMQComponent.setConnectionFactory(factory);
+        return activeMQComponent;
+    }
+}
+
+
