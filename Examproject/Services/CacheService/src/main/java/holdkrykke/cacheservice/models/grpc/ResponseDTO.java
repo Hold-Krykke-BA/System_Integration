f01f74fd665823d47549fe3e2b2b207a10a0ff7c
Runi <Runiniclasen@hotmail.com>
2021-12-16 17:08:34 +0100

    initial Constructors

--- Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/models/grpc/ResponseDTO.java
+++ Examproject/Services/CacheService/src/main/java/holdkrykke/cacheservice/models/grpc/ResponseDTO.java
@@ -1,5 +1,9 @@
 package holdkrykke.cacheservice.models.grpc;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import holdkrykke.cacheservice.models.Book.Book;
+import holdkrykke.cacheservice.models.redis.BookCacheDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +22,38 @@ public class ResponseDTO implements Serializable {
     private Double price;
     //below is different from OrderItem
     private int quantity;
+
+    /**
+     * Create a ResponseDTO from a BookCacheDTO
+     *
+     * @param dto
+     */
+    public ResponseDTO(BookCacheDTO dto) {
+        this.isbn = dto.getIsbn();
+        this.title = dto.getTitle();
+        this.authors = dto.getAuthors();
+        this.location = dto.getLocation();
+        this.type = dto.getType();
+        this.price = dto.getPrice();
+        this.quantity = dto.getQuantity();
+    }
+
+    /**
+     * Create a ResponseDTO from a Book
+     *
+     * @param book
+     */
+    public ResponseDTO(Book book) {
+        this.isbn = book.getIsbn();
+        this.title = book.getTitle();
+        this.authors = book.getAuthors();
+        this.location = book.getLocation();
+        this.type = book.getType();
+        this.price = book.getPrice();
+        this.quantity = book.getQuantity();
+    }
+
+    public ResponseDTO(JsonObject intermediary) {
+        //intermediary.get("blah");
+    }
 }
